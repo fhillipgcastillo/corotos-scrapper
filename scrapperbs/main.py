bf33b33a4231c3e4831859067a42896b4a90b3bf
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2019-08-25 02:42:02 -0400

    add multiprocessing and need to fix or figure out requestion different fromt he main apge

--- scrapperbs/main.py
+++ scrapperbs/main.py
@@ -1,9 +1,10 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as bsoup
 from productModelBase import ProductModelBase
+from multiprocessing import Pool
 import re
 import requests
-
+from time import sleep
 
 def getHtmlContentWithUrlOpen(url):
   #open connection and reads content
@@ -19,26 +20,31 @@ def getHtmlContentWithUrlOpen(url):
 def getHtmlContentWithRequests(url):
   # using requests lib
   headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    'referer': url,
+    'Cache-Control': 'no-cache'
   }
   content = ""
   try:
-    req = requests.get(url, headers=headers, timeout=10)
+    req = requests.get(url, headers=headers, timeout=10, stream=True)
+    # sleep(2)
+    print ("status_code", req.status_code)
     if req.status_code == 200:
       content = req.text
+    else: 
+      print("got not data")
+
   except Exception as ex:
       print(str(ex))
-  finally:
-      print("finished with requests lib")
-
+  
   return content
 
-def Main():
-  url = "https://www.corotos.com.do"
-  # content = getHtmlContentWithUrlOpen(url)
-  
+def Main(url):
+  print(url)
   # More faster that UrlOpen
+  # content = getHtmlContentWithUrlOpen(url)
   content = getHtmlContentWithRequests(url)
+  # content = tempContent[0]
 
   # parse content to soup
   soupedContent = bsoup(content, "html.parser")
@@ -56,8 +62,14 @@ def Main():
 
   # container class "div", {"class":"_3_VJn _2h9MX"}
   # container = soupedContent.find("div", {"class":"_3_VJn _2h9MX"})
-  # print(dir(container))
+
   products = soupedContent.findAll("div", {"class":"DbXTC _2pm69 _1JgR4 QF_XG"})
+
+  if len(products) == 0:
+    content = getHtmlContentWithUrlOpen(url)
+    soupedContent = bsoup(content, "html.parser")
+    products = soupedContent.findAll("div", {"class":"DbXTC _2pm69 _1JgR4 QF_XG"})  
+
   print("Found products ", len(products))
   # print("thumbnail", dir(products[0].a.div.find("div", {"class":"_1xvFo"})["style"]))
   # /listings/celulares-y-tabletas-186/25c04925-8643-4156-a7e9-b3b35491f211/iphone-8-plus-64gb-factory
@@ -88,10 +100,6 @@ def Main():
     productObject = ProductModelBase(titleTag.string, priceTag.string, href, fullLink, hashId, thumbnailUrl, category)
     
     # print(productObject)
-    # print(thumbnailUrl)
-    # print("thumbnail", dir(thumbnailDiv["style"]))
-    # print(fullLink)
-    
     # load more button " button data-name="load_more""
   print("finished")
   
@@ -106,5 +114,20 @@ def getHashId(listingUrl=""):
   _, listingStr, category, hashId, title = splitListingUrl(listingUrl)
   return hashId or ""
 
-Main()
+if __name__ == '__main__':
+  
+  urls = [
+    # "https://www.corotos.com.do/sc/electr%C3%B3nica/celulares-tabletas",
+    # "https://www.corotos.com.do/sc/electr%C3%B3nica/computadoras-laptops",
+    # "https://www.corotos.com.do/c/inmuebles-en-venta",
+    "https://www.corotos.com.do/"
+  ]
+  # Main("https://www.corotos.com.do")
+  with Pool(4) as pool:
+    try:
+      tempContent = pool.map(Main, urls)
+    except Exception as ex:
+      print(ex)
+      
+    
 # sleep for x time and re-run
