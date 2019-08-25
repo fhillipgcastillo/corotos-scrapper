cf9a61a21b0328115b7df11daac24789a8d50f17
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2019-08-24 23:27:21 -0400

    be able to run scrapper and list object found

--- scrapperbs/main.py
+++ scrapperbs/main.py
@@ -1,50 +1,79 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as bsoup
 from productModelBase import ProductModelBase
+import re
 
-url = "https://www.corotos.com.do"
+def Main():
+  url = "https://www.corotos.com.do"
 
-#open connection and reads content
-client = uReq(url)
-content = client.read()
+  #open connection and reads content
+  client = uReq(url)
+  content = client.read()
 
-# close connection
-client.close()
+  # close connection
+  client.close()
 
-# parse content to soup
-soupedContent = bsoup(content, "html.parser")
+  # parse content to soup
+  soupedContent = bsoup(content, "html.parser")
 
-# to find
-# soupedContent.findAll("<tagname>", {"property":"value"})
-#for example
-# soupedContent.findAll("div", {"class":"item"})
+  # to find
+  # soupedContent.findAll("<tagname>", {"property":"value"})
+  #for example
+  # soupedContent.findAll("div", {"class":"item"})
 
-# using parse, tag["property"] = value
-# soupedContent.div.div["property"]
-# example:
-# soupedContent.div.img["src"]
+  # using parse, tag["property"] = value
+  # soupedContent.div.div["property"]
+  # example:
+  # soupedContent.div.img["src"]
 
-# container class "div", {"class":"_3_VJn _2h9MX"}
-# container = soupedContent.find("div", {"class":"_3_VJn _2h9MX"})
-# print(dir(container))
-products = soupedContent.findAll("div", {"class":"DbXTC _2pm69 _1JgR4 QF_XG"})
-print("Found products ", len(products))
-# print("thumbnail", dir(products[0].a.div.find("div", {"class":"_1xvFo"})["style"]))
-productObjects = []
-for product in products:
-  # href format
-  # /listings/<category-<id>>/<publication id>/<posible title -<id>
-  href = product.a["href"]
-  fullLink = url + href
-  thumbnailDiv = product.a.div.find("div", {"class":"_1xvFo"})
-  infoDiv = product.a.div.find("div", {"class":"_32PML"})
-  priceTag = infoDiv.find("span",{"data-name":"price"})
-  titleTag = infoDiv.find("h2",{"data-name":"ad-title"})
-  productObject = ProductModelBase(titleTag.string, priceTag.string, href, fullLink)
-  print(productObject)
+  # container class "div", {"class":"_3_VJn _2h9MX"}
+  # container = soupedContent.find("div", {"class":"_3_VJn _2h9MX"})
+  # print(dir(container))
+  products = soupedContent.findAll("div", {"class":"DbXTC _2pm69 _1JgR4 QF_XG"})
+  print("Found products ", len(products))
+  # print("thumbnail", dir(products[0].a.div.find("div", {"class":"_1xvFo"})["style"]))
+  # /listings/celulares-y-tabletas-186/25c04925-8643-4156-a7e9-b3b35491f211/iphone-8-plus-64gb-factory
 
-  # print("thumbnail", dir(thumbnailDiv["style"]))
-  # print(fullLink)
+  productObjects = []
 
+  for product in products:
+    # href format
+    # /listings/<category-<id>>/<publication id>/<posible title -<id>
+    href = product.a["href"]
+    fullLink = url + href
+    hashId = getHashId(href)
+    category = splitListingUrl(href)[2] or ""
 
-# load more button " button data-name="load_more""
+    thumbnailDiv = product.a.div.find("div", {"class":"_1xvFo"})
+    thumbailStyle = thumbnailDiv["style"]
+    thumbnailUrl = ""
+
+    found = re.search('background-image:url\((.*)\)', thumbailStyle, re.IGNORECASE)
+    if found:
+      thumbnailUrl = found.group(1)
+
+    infoDiv = product.a.div.find("div", {"class":"_32PML"})
+    priceTag = infoDiv.find("span",{"data-name":"price"})
+    titleTag = infoDiv.find("h2",{"data-name":"ad-title"})
+
+    productObject = ProductModelBase(titleTag.string, priceTag.string, href, fullLink, hashId, thumbnailUrl, category)
+    
+    print(productObject)
+    # print(thumbnailUrl)
+    # print("thumbnail", dir(thumbnailDiv["style"]))
+    # print(fullLink)
+    
+    # load more button " button data-name="load_more""
+
+def splitListingUrl(listingUrl):
+  return listingUrl.split("/")
+
+def getHashId(listingUrl=""):
+  # /listings/category/hash-id/title
+  # for example: 
+  # /listings/celulares-y-tabletas-186/25c04925-8643-4156-a7e9-b3b35491f211/iphone-8-plus-64gb-factory
+  
+  _, listingStr, category, hashId, title = splitListingUrl(listingUrl)
+  return hashId or ""
+
+Main()
--- scrapperbs/productModelBase.py
+++ scrapperbs/productModelBase.py
@@ -1,13 +1,15 @@
 class ProductModelBase:
-  def __init__(self, title="", price="", listingPath="", fullUrl="", hashId=""):
+  def __init__(self, title="", price="", listingPath="", fullUrl="", hashId="", thumbnailUrl="", category=""):
     self.hashId = hashId
     self.title = title
     self.price = price
     self.listingPath = listingPath
     self.fullUrl = fullUrl
-  
+    self.thumbnailUrl = thumbnailUrl
+    self.category = category
+
   def __str__(self):
     return self.hashId+" "+self.title
 
   def getJsonString(self):
-    return "{title:"+self.title+", price:"+self.price+", hashId:"+self.hashId+", listingPath:"+self.listingPath+", fullUrl:"+self.fullUrl+", }"
+    return "{title:"+self.title+", price:"+self.price+", hashId:"+self.hashId+", listingPath:"+self.listingPath+", fullUrl:"+self.fullUrl+", thumbnailUrl:"+self.thumbnailUrl+" }"
